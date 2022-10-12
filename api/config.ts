52b508388e26df01ea9be398fee1dc421752d9a2
Hexi <66669483+Hexi1997@users.noreply.github.com>
2022-10-12 01:45:09 +0000

    Update config.ts

--- api/config.ts
+++ api/config.ts
@@ -6,7 +6,7 @@
  * ! Don't put your privateKey inside this file directly!
  */
 export default {
-  viewId: 'TODO:205703100',
+  viewId: '4137951381',
   auth: {
     projectId: 'market-visit-count',
     privateKey: process.env.PRIVATE_KEY,
